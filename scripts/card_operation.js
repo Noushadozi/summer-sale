6256f2df4932007c8698f7f53b0d4ea6b93ec507
Noushadozi <noushadozi333@gmail.com>
2023-08-20 15:32:55 +0600

    9 cards added

--- scripts/card_operation.js
+++ scripts/card_operation.js
@@ -3,46 +3,152 @@ let totalPrice = 0;
 
 document.getElementById('card-1').addEventListener('click', function(){
     const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-1").innerText;
     const p = document.createElement('p');
     p.classList.add('text-[#111]');
     p.classList.add('text-[24px]');
     p.classList.add('pl-[24px]');
     p.classList.add('pt-[20px]');
     p.classList.add('font-semibold');
-    p.innerText = `${purchasedItemsNumbers}. K. Accessories`;
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
     purchasedItems.appendChild(p);
     purchasedItemsNumbers++;
     const price = parseFloat(document.getElementById('price-1').innerText);
     totalPrice += price;
     document.getElementById('total-price').innerText = totalPrice;
 })
+
+
 document.getElementById('card-2').addEventListener('click', function(){
     const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-2").innerText;
     const p = document.createElement('p');
     p.classList.add('text-[#111]');
     p.classList.add('text-[24px]');
     p.classList.add('pl-[24px]');
     p.classList.add('pt-[20px]');
     p.classList.add('font-semibold');
-    p.innerText = `${purchasedItemsNumbers}. K. Accessories`;
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
     purchasedItems.appendChild(p);
     purchasedItemsNumbers++;
     const price = parseFloat(document.getElementById('price-2').innerText);
     totalPrice += price;
     document.getElementById('total-price').innerText = totalPrice;
 })
+
+
 document.getElementById('card-3').addEventListener('click', function(){
     const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-3").innerText;
     const p = document.createElement('p');
     p.classList.add('text-[#111]');
     p.classList.add('text-[24px]');
     p.classList.add('pl-[24px]');
     p.classList.add('pt-[20px]');
     p.classList.add('font-semibold');
-    p.innerText = `${purchasedItemsNumbers}. Home Cooker`;
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
     purchasedItems.appendChild(p);
     purchasedItemsNumbers++;
     const price = parseFloat(document.getElementById('price-3').innerText);
     totalPrice += price;
     document.getElementById('total-price').innerText = totalPrice;
 })
+document.getElementById('card-4').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-4").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-4').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+document.getElementById('card-5').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-5").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-5').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+document.getElementById('card-6').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-6").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-6').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+document.getElementById('card-7').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-7").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-7').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+
+document.getElementById('card-8').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-8").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-8').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+
+document.getElementById('card-9').addEventListener('click', function(){
+    const purchasedItems = document.getElementById('purchased-items');
+    const productName = document.getElementById("product-name-9").innerText;
+    const p = document.createElement('p');
+    p.classList.add('text-[#111]');
+    p.classList.add('text-[24px]');
+    p.classList.add('pl-[24px]');
+    p.classList.add('pt-[20px]');
+    p.classList.add('font-semibold');
+    p.innerText = `${purchasedItemsNumbers}. ${productName}`;
+    purchasedItems.appendChild(p);
+    purchasedItemsNumbers++;
+    const price = parseFloat(document.getElementById('price-9').innerText);
+    totalPrice += price;
+    document.getElementById('total-price').innerText = totalPrice;
+})
+
