8bc25c041879e326456ea556df8a9b227bcde4cb
Noushadozi <noushadozi333@gmail.com>
2023-08-20 15:05:35 +0600

    total added

--- scripts/index.js
+++ scripts/index.js
@@ -8,13 +8,17 @@ document
       console.log(e.target.value);
     }
   });
-document.getElementById('coupon-input-field-button').addEventListener('click', function(){
-    const totalPriceString = document.getElementById('total-price').innerText;
+document
+  .getElementById("coupon-input-field-button")
+  .addEventListener("click", function () {
+    const totalPriceString = document.getElementById("total-price").innerText;
     const totalPrice = parseFloat(totalPriceString);
-    if(totalPrice < 200){
-        alert('Minimum purchased required TK 200 or above to get discount');
-        return;
+    if (totalPrice < 200) {
+      alert("Minimum purchased required TK 200 or above to get discount");
+      return;
     }
     const discount = totalPrice * 0.2;
     document.getElementById("discount").innerText = discount;
-})
+    const total = totalPrice - discount;
+    document.getElementById("total").innerText = total;
+  });
